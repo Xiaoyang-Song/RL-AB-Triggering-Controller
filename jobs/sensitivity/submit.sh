dafaf33ff6cd38c4e98fbef63602511db972c09a
Xiaoyang-Song <xysong@umich.edu>
2026-05-06 15:29:19 -0400

    Sync: new results

--- jobs/sensitivity/submit.sh
+++ jobs/sensitivity/submit.sh
@@ -1,9 +1,13 @@
 
 
-sbatch jobs/sensitivity/j0.sh
-sbatch jobs/sensitivity/j1.sh
-sbatch jobs/sensitivity/j2.sh
-sbatch jobs/sensitivity/j3.sh
-sbatch jobs/sensitivity/j4.sh
-sbatch jobs/sensitivity/j5.sh
-sbatch jobs/sensitivity/j6.sh
+# sbatch jobs/sensitivity/j0.sh
+# sbatch jobs/sensitivity/j1.sh
+# sbatch jobs/sensitivity/j2.sh
+# sbatch jobs/sensitivity/j3.sh
+# sbatch jobs/sensitivity/j4.sh
+# sbatch jobs/sensitivity/j5.sh
+# sbatch jobs/sensitivity/j6.sh
+
+sbatch jobs/sensitivity/j7.sh
+sbatch jobs/sensitivity/j8.sh
+sbatch jobs/sensitivity/j9.sh
